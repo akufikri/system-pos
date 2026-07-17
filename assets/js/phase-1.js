78e92d7928429357a118d31a25a780795f0154c7
Fikri <fikrinurhakim911@gmail.com>
2026-07-17 22:17:53 +0700

    feat: add initial JavaScript file and HTML structure for learning phase 1

---
+++ assets/js/phase-1.js
@@ -0,0 +1,46 @@
+// Variabel :
+
+// 1. const (const adalah variabel yang tidak bisa di ubah valuenya);
+// const priceProduct = 100
+// console.log("Harga Produk", priceProduct)
+
+// 2. let
+// let qtyCart = 10;
+// console.log("Qty Cart Produk : ", qtyCart)
+
+// 3 var
+// var nameUsers = "Budi"
+// console.log("Nama User : ", nameUsers)
+
+// sample variabel operator matematika :
+// const qtyProduct = 10
+// const priceProducts = 3000
+
+// let total = qtyProduct + priceProducts
+// console.log(total)
+
+// Tipe data :
+
+// 1. string (string adalah value bisa text, number, ataupun karakter khusus)
+// let exampleString1 = "Bpk.Budi";
+// let exampleString2 = "Dari batam";
+
+// 2. number
+// let exampleNumber1 = 10;
+// let exampleNumber2 = 300;
+
+// 3. bolean
+// let exampleBoolean1 = true; // true
+// let exampleBoolean2 = false; // false
+
+// 4. array
+// let cartProduct = [
+//   { name: "Kopi susu", qty: 2, price: 18000, status: true }, // 0
+//   { name: "Kopi gula aren", qty: 0, price: 28000, status: false }, // 1
+//   { name: "Kopi gula aren 2", qty: 0, price: 28000, status: false }, // 2
+// ];
+
+// 5. object
+// let productItem = {
+//   "example 1": { id: "PROD-001", name: "Kopi susu gula aren", price: 25000 },
+// };
